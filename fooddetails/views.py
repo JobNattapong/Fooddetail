d5209403348513ed2e61dbb19577c78ccc35f33f
jobnattapong <s5701012620187@email.kmutnb.ac.th>
2018-06-29 15:04:15 +0700

    run function test error

--- fooddetails/views.py
+++ fooddetails/views.py
@@ -4,16 +4,22 @@ from fooddetails.models import Food
 
 # Create your views here.
 def homepage(request):
+    error_message = ""
     data_food = Food.objects.all()
-    return render(request, 'fooddetails/homepage.html', {'foods' : data_food})
+    return render(request, 'fooddetails/homepage.html', {'foods' : data_food, 'error_message' : error_message})
 
 def add_food(request):
-    Food.objects.create(
-    food_text = request.POST['nameFood'],
-    sugar = int(request.POST['sizeProtein']),
-    protein = int(request.POST['sizeSugar']),
-        )
-    return redirect('/')
+    error_message = ""
+    data_food = Food.objects.all()
+    food_ = str(request.POST['nameFood'])
+    sugar_ = request.POST['sizeSugar']
+    protein_ = request.POST['sizeProtein']
+    if (food_ != '' and sugar_ != '' and protein_ != ''):
+        Food.objects.create(food_text = food_ ,sugar = sugar_ ,protein = protein_)
+        return redirect('/')
+    else:
+        error_message = "please enter fully"
+        return render(request, 'fooddetails/homepage.html', {'foods' : data_food, 'error_message' : error_message})
 
 def delete_row_table(request):
     del_id = int(request.POST['del_id'])
--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -28,41 +29,41 @@ class NewVisitorTest(unittest.TestCase):
         nameFoof.send_keys('กระเพาไก่')
         time.sleep(1)
 
-        sizeProtein = self.browser.find_element_by_id('size_protein')
-        sizeProtein.send_keys(24)
-        time.sleep(1)
-
         sizeSugar = self.browser.find_element_by_id('size_sugar')
         sizeSugar.send_keys(5)
+        time.sleep(1)
+
+        sizeProtein = self.browser.find_element_by_id('size_protein')
+        sizeProtein.send_keys(24)
         time.sleep(1)
 
         # เขาได้ทำการใส่ข้อมูลลงไปและกดเพิ่ม
         self.browser.find_element_by_id("id_sub").click()
         time.sleep(1)
         # เขาจะเห็นว่ารายละเอียดที่กรอกเข้าไปจะไปอยู่ใน list แต่จะขึ้นแค่ชื่ออาหาร
-        self.check_for_row_in_list_table('กระเพาไก่ 24 5')
+        self.check_for_row_in_list_table('กระเพาไก่ 5 % 24 %')
 
         nameFoof = self.browser.find_element_by_id('name_food')
         nameFoof.send_keys('ราดหน้า')
-        time.sleep(1)
-
-        sizeProtein = self.browser.find_element_by_id('size_protein')
-        sizeProtein.send_keys(6)
         time.sleep(1)
 
         sizeSugar = self.browser.find_element_by_id('size_sugar')
         sizeSugar.send_keys(30)
         time.sleep(1)
 
+        sizeProtein = self.browser.find_element_by_id('size_protein')
+        sizeProtein.send_keys(6)
+        time.sleep(1)
+
         # เขาได้ทำการใส่ข้อมูลลงไปและกดเพิ่ม
         self.browser.find_element_by_id("id_sub").click()
         time.sleep(1)
 
-        self.check_for_row_in_list_table('กระเพาไก่ 24 5')
-        self.check_for_row_in_list_table('ราดหน้า 6 30')
+        self.check_for_row_in_list_table('กระเพาไก่ 5 % 24 %')
+        self.check_for_row_in_list_table('ราดหน้า 30 % 6 %')
         # และเขาสามารถลบข้อมูลที่เพิ่มเข้าไปได้เช่นลบ กระเพาไก่
         delete = self.browser.find_element_by_id('del_input')
-        delete.send_keys(1)
+        delete.send_keys(10)
         time.sleep(1)
 
         self.browser.find_element_by_id("del").click()
@@ -70,10 +71,10 @@ class NewVisitorTest(unittest.TestCase):
         # เช็คว่า data1 หายจริงๆ
         table = self.browser.find_element_by_id('id_list_table')
         rows_text = table.find_elements_by_tag_name('tr')
-        self.assertNotIn('กระเพาไก่ 24 5 Delete', [row.text for row in rows_text])
+        self.assertNotIn('กระเพาไก่ 5 % 24 %', [row.text for row in rows_text])
         time.sleep(1)
         # ดูข้อมูลที่เหลือ
-        self.check_for_row_in_list_table('1: ราดหน้า 6 30 Delete')
+        self.check_for_row_in_list_table('ราดหน้า 6 30')
         time.sleep(1)
 
         self.fail('Finish the test!')
